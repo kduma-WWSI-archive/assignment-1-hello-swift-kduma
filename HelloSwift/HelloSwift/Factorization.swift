2ee13c6aa871086a4d1c76afe6944706dac29d0a
Krystian Duma <git@krystian.duma.sh>
2019-11-13 23:14:24 +0100

    Implement factorize function
    StackOverflow FTW 🦄

--- HelloSwift/HelloSwift/Factorization.swift
+++ HelloSwift/HelloSwift/Factorization.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class Factorization {
+    enum Result: Equatable {
+        case prime
+        case composite(factors: [Int])
+    }
+
+    static func factorize(_ number: Int) -> Result? {
+        var number = number
+        var factors: [Int] = []
+
+        var divisor = 2
+        while divisor * divisor <= number {
+            while number % divisor == 0 {
+                factors.append(divisor)
+                number /= divisor
+            }
+            divisor += divisor == 2 ? 1 : 2
+        }
+
+        if number > 1 {
+            factors.append(number)
+        }
+
+        if(factors.count == 0){
+            return nil
+        }
+
+        if(factors.count == 1){
+            return .prime
+        }
+
+        return .composite(factors: factors)
+    }
+}
